8d60bc3da65421ce32a24d73a136efbf508518d3
Jaeyeon <wodus5677@gmail.com>
2019-12-18 21:24:19 +0900

    naver map api location tracking mode on

--- app/src/main/java/com/jaeyeon/expeditiouscopyorderapp/ui/mySurrounding/MySurroundingFragment.kt
+++ app/src/main/java/com/jaeyeon/expeditiouscopyorderapp/ui/mySurrounding/MySurroundingFragment.kt
@@ -5,14 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.UiThread
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.jaeyeon.expeditiouscopyorderapp.R
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
 
-class MySurroundingFragment : Fragment() {
+class MySurroundingFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var mySurroundingViewModel: MySurroundingViewModel
+    private lateinit var locationSource : FusedLocationSource
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,10 +29,40 @@ class MySurroundingFragment : Fragment() {
         mySurroundingViewModel =
             ViewModelProviders.of(this).get(MySurroundingViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_my_surrounding, container, false)
-        //val textView: TextView = root.findViewById(R.id.text_surrounding)
-        //mySurroundingViewModel.text.observe(this, Observer {
-        //    textView.text = it
-        //})
+
+        val fm = childFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map, it).commit()
+            }
+
+        mapFragment.getMapAsync(this)
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+
+
         return root
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions,
+                grantResults)) {
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    @UiThread
+    override fun onMapReady(naverMap: NaverMap) {
+        val uiSettings = naverMap.uiSettings
+        uiSettings.isZoomControlEnabled = false
+        naverMap.locationSource = locationSource
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
